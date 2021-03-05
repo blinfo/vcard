e7a8f612c9321e5ea5f073e6fef236852c0b5fbc
hakan <hakan@NL579>
2021-03-05 09:41:28 +0100

    Initiated

---
+++ vcard/src/main/java/vcard/Person.java
@@ -0,0 +1,148 @@
+package vcard;
+
+/**
+ *
+ * @author Håkan Lidén
+ */
+public class Person {
+
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String title;
+    private String title1;
+    private String organisation;
+    private String title2;
+    private String photo;
+    private String mediaType;
+    private String phoneWork;
+    private String phoneHome;
+    private String addressWork;
+    private String addressHome;
+    private String email1;
+    private String revision;
+
+    public Person() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle1() {
+        return title1;
+    }
+
+    public void setTitle1(String title1) {
+        this.title1 = title1;
+    }
+
+    public String getOrganisation() {
+        return organisation;
+    }
+
+    public void setOrganisation(String organisation) {
+        this.organisation = organisation;
+    }
+
+    public String getTitle2() {
+        return title2;
+    }
+
+    public void setTitle2(String title2) {
+        this.title2 = title2;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public String getPhoneWork() {
+        return phoneWork;
+    }
+
+    public void setPhoneWork(String phoneWork) {
+        this.phoneWork = phoneWork;
+    }
+
+    public String getPhoneHome() {
+        return phoneHome;
+    }
+
+    public void setPhoneHome(String phoneHome) {
+        this.phoneHome = phoneHome;
+    }
+
+    public String getAddressWork() {
+        return addressWork;
+    }
+
+    public void setAddressWork(String addressWork) {
+        this.addressWork = addressWork;
+    }
+
+    public String getAddressHome() {
+        return addressHome;
+    }
+
+    public void setAddressHome(String addressHome) {
+        this.addressHome = addressHome;
+    }
+
+    public String getEmail1() {
+        return email1;
+    }
+
+    public void setEmail1(String email1) {
+        this.email1 = email1;
+    }
+
+    public String getRevision() {
+        return revision;
+    }
+
+    public void setRevision(String revision) {
+        this.revision = revision;
+    }
+
+}
